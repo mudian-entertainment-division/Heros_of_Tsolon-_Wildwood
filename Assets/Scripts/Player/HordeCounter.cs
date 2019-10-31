1ce09347725d04614f9353415f2618f1439e1a2e
JamesNemeth <53249899+JamesNemeth@users.noreply.github.com>
2019-10-31 15:53:42 +1100

    Changed somne UI, added dropable coins, mana changes and regenerates

---
+++ Assets/Scripts/Player/HordeCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HordeCounter : MonoBehaviour
+{
+    public static int score;
+    public static Text scoreDisplay;
+
+    private void Start()
+    {
+        scoreDisplay = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+        InvokeRepeating("Timer", 1, 0.25f);
+    }
+
+    public static void Increase()
+    {
+        scoreDisplay.text = "Score: " + score;
+    }
+}
---
+++ Assets/Scripts/Player/RadialMana.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialMana : MonoBehaviour
+{
+    public Image radialManaIcon;
+    public float curMana, maxMana;
+
+    bool ManaUsed;
+    public float ManaPerSecond = 1f;
+
+    private void Start()
+    {
+        InvokeRepeating("Timer", 1, 1f);
+    }
+
+    void ManaChange()
+    {
+        float amount = Mathf.Clamp01(curMana / maxMana);
+        radialManaIcon.fillAmount = amount;
+    }
+    void Update()
+    {
+        ManaChange();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            curMana -= 1;
+        }
+
+        if (curMana > 10)
+        {
+            curMana = 10;
+        }
+    }
+    private void Timer()
+    {
+        curMana += ManaPerSecond;
+    }
+}
