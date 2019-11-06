5b248feafa7eb7f99f2498d7ed234ada42370150
JamesNemeth <53249899+JamesNemeth@users.noreply.github.com>
2019-11-06 14:32:25 +1100

    Merged assets from USB version

--- Assets/Scripts/Player/PlayerHandler.cs
+++ Assets/Scripts/Player/PlayerHandler.cs
@@ -1,50 +1,54 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerHandler : MonoBehaviour
 {
-    [System.Serializable]
-    public struct PlayerStats
-    {
-        public string name;
-        public int value;
-    }
-
     [Header("Value Variables")]
 
     public float curHealth;
-    public float curMana, maxHealth, maxMana;
+    public float curEnergy, maxHealth, maxEnergy;
+    public float energyPerSecond = 1f;
+    public float healthPerSecond = 1f;
 
-    [Header("Value Icons")]
+    [Header("Value Variables")]
 
     public Image radialHealthIcon;
-    public Image radialManaIcon;
+    public Image radialEnergyIcon;
 
-    [Header("Damage Effects")]
+    [Header("Damage Effect Variables")]
 
     public Image damageImage;
     public float flashSpeed = 5;
-    public Color flashColor = new Color(1, 0, 0, 0.2f);
+    public Color flashColor = new Color(1, 1, 0, 0.2f);
     bool damaged;
 
+    private void Start()
+    {
+        InvokeRepeating("Timer", 1, 1f);
+    }
     void Update()
     {
         HealthChange();
-        ManaChange();
+
+        EnergyChange();
 
         void HealthChange()
         {
             float amount = Mathf.Clamp01(curHealth / maxHealth);
             radialHealthIcon.fillAmount = amount;
-        }
-        void ManaChange()
-        {
-            float amount = Mathf.Clamp01(curMana / maxMana);
-            radialManaIcon.fillAmount = amount;
-        }
 
+            if (curHealth > 245)
+            {
+                curHealth = 245;
+            }
+            if (curHealth > 245)
+            {
+                curHealth = 245;
+            }
+        }
         if (Input.GetKeyDown(KeyCode.A))
         {
             damaged = true;
@@ -59,5 +63,28 @@ public class PlayerHandler : MonoBehaviour
         {
             damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
         }
+
+        void EnergyChange()
+        {
+            float amount = Mathf.Clamp01(curEnergy / maxEnergy);
+            radialEnergyIcon.fillAmount = amount;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            curEnergy -= 1;
+        }
+
+        if (curEnergy > 10)
+        {
+            curEnergy = 10;
+        }
+
     }
+    private void Timer()
+    {
+        curEnergy += energyPerSecond;
+        curHealth += healthPerSecond;
+    }
+    
 }
+
