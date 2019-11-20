bb2ac55322cbbf644ca0f47e0ec11feb786f3d8c
JamesNemeth <53249899+JamesNemeth@users.noreply.github.com>
2019-11-20 15:01:37 +1100

    Added colored map, building build on a key input,

---
+++ Assets/Scripts/Tower/BuildingTower.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingTower : MonoBehaviour
+{
+    public Text textElement;
+    public GameObject Tower;
+
+    private void Awake()
+    {
+        textElement.gameObject.SetActive(false);
+        Tower.gameObject.SetActive(false);
+    }
+    void OnTriggerEnter(Collider BuildingTrigger)
+    {
+        textElement.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider BuildingTrigger)
+    {
+         textElement.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (textElement.gameObject.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                textElement.gameObject.SetActive(false);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Tower.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Tower/Canon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : BuildingTower
+{
+    public GameObject canonTower;
+
+    private void Awake()
+    {
+        canonTower.gameObject.SetActive(false);
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            canonTower.gameObject.SetActive(true);
+        }
+    }
+}
